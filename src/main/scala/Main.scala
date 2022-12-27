88c3880cef598b7d6ea36352acd6d2e819f6140f
ersingulbahar <ersin.gulbahar@gmail.com>
2022-12-27 10:53:22 +0000

    ersing +

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,12 @@
-import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
-import java.nio.file.Paths
 
 object Main {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().master("local").appName("SparExample").getOrCreate();
     val rdd = spark.sparkContext.textFile("C:\\Users\\hasanozka\\IdeaProjects\\scala-spark\\data\\foo.txt");
     spark.sparkContext.setLogLevel("WARN")
+    //System.setProperty("hadoop.home.dir", "C:\\Users\\hasanozka\\IdeaProjects\\scala-spark\\hadoop_home\\hadoop-2.7.1\\bin\\")
     println("Count : " + rdd.count())
     println("First : " + rdd.first())
 
@@ -15,13 +14,12 @@ object Main {
     val twoRecord = rdd.take(2);
     for (line <- twoRecord)
       println("-------> " + line)
-    System.setProperty("hadoop.home.dir", Paths.get("winutils").toAbsolutePath.toString)
 
     val filteredRDD = rdd.filter(line => line.contains("foo1"))
     println("----------------------- > Filtered Count : " +
       filteredRDD.count())
     filteredRDD.foreach(line => println("-------------------------->" + line))
-    filteredRDD.saveAsTextFile("C:\\Users\\hasanozka\\IdeaProjects\\scala-spark\\data\\filtered_result")
+    //filteredRDD.saveAsTextFile("C:\\Users\\hasanozka\\IdeaProjects\\scala-spark\\data\\filtered_result")
 
 
     val rdd_movies = spark.sparkContext.textFile("C:\\Users\\hasanozka\\IdeaProjects\\scala-spark\\data\\movies.csv");
@@ -34,9 +32,19 @@ object Main {
     println("Movie RDD Count : " + movieRdd.count())
     println("Movie RDD First : " + movieRdd.first())
 
+  //flatmap
 
+    val movieRddFlat = rdd_movies.flatMap(row => {
+      row.split(",")
+    })
+    println("Flatmap RDD Count : " + movieRddFlat.count())
+    println("Flatmap RDD First : " + movieRddFlat.first())
 
+  //sample
 
+    val movieRddSample = rdd_movies.sample(false, 0.9)
+    println("Sample Movie RDD Count : " + movieRddSample.count())
+    println("Sample RDD First : " + movieRddSample.first())
   }
 }
 
